bdcd2f45626f368475f2f20074edb5edfd913a3c
Anthony <anthonyrandomcarey@gmail.com>
2020-05-30 11:25:56 -0400

    Actual last commit.

--- NGG.py
+++ NGG.py
@@ -1,39 +1,49 @@
 import random
 
 
-first = 1
-last = 10
+def startGame():
+    # Welcome the user
+    print('\nWelcome to the Number Guessing Game!\n')
 
-def game():
-    print('------------------------------------')
-    print('Welcome to the Number Guessing Game!')
-    print('------------------------------------')
-    print('')
-
-    randomNum = random.randint(first, last)
+    # Variables
+    gameBegin = True
+    highScore = ''
     score = 0
-    guess = ''
-    while guess != randomNum:
+    answer = random.randint(1, 10)
+
+    # Game begins
+    while(gameBegin):
         try:
-            guess = int(input('Pick a number between {} and {}: '.format(first, last)))
-            score += 1
-            if guess < first or guess > last:
-                raise ValueError('Please use a number between {} and {}.'.format(first, last))
-        except ValueError as err:
+            guess = int(input('Input your guess between 1 and 10: '))
+            if guess > 10 or guess < 1:
+                score += 1
+                print('\nYour guess needs to be between 1 and 10.\n')
+            elif guess > answer:
+                score += 1
+                print('\nToo high! Try again.\n')
+            elif guess < answer:
+                score += 1
+                print('\nToo low! Try again.\n')
+            elif guess == answer:
+                score += 1
+                gameBegin = False
+                if highScore == '' or highScore > score:
+                    highScore = score
+                print('\nYou got it! It took you {} tries.\n'.format(score))
+                # Exceeds
+                while(True):
+                    startAgain = input('Want to play again? [Y/N]: ')
+                    if startAgain[0].lower() == 'y':
+                        gameBegin = True
+                        score = 0
+                        answer = random.randint(1, 10)
+                        print('\nThe current high score is: {}\n'.format(highScore))
+                        break
+                    else:
+                        print('\nThanks for playing!\nClosing game...')
+                        quit()
+        except ValueError:
             print('Something went wrong...')
-            print(err)
-            continue
-        if guess > randomNum:
-            print('Too high! Try again.')
-        elif guess < randomNum:
-            print('Too low! Try again.')
-        else:
-            highScore = score
-            print('You got it! It took you {} tries.'.format(score))
-            print('Highscore is now {} tries.'.format(highScore))
-    if score < highScore:
-        print('=-=-=- New High Score! -=-=-=')
-        highScore = score
+    # Game ends
 
-
-game()
+startGame()
